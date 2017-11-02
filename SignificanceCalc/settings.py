999aed593d3c28fab2e7868392509308b76a5b4b
3loader <abdulinaleks@gmail.com>
2017-11-02 23:06:56 +0200

    staticfiles fix 2

--- SignificanceCalc/settings.py
+++ SignificanceCalc/settings.py
@@ -23,7 +23,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'site_static')
+    os.path.join(PROJECT_ROOT, 'staticfiles')
 ]
 
 LOCALE_PATHS = [
